9aca65dc0c2cca648f525173545e6b4e9b8a2b0f
Marko007205 <markouljarevic16@gmail.com>
2022-10-20 17:40:44 +0200

    Create integration and login folders

---
+++ src/app/integration/login/int-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-int-login',
+  templateUrl: './int-login.component.html',
+  styleUrls: ['./int-login.component.css']
+})
+export class IntLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
