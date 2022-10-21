735ed366940afae2c8ca342073bc62c8bf94e4eb
TheWhiteRabbit21 <milutinovicn21@gmail.com>
2022-10-21 17:51:32 +0200

    dodato rutiranje i welcome page

--- src/app/modules/pages/welcome/welcome.component.ts
+++ src/app/modules/pages/welcome/welcome.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  //styleUrls: ['css/styles.css']
 })
 export class WelcomeComponent implements OnInit {
+
+  //PageTitle: string = "Welcome Page";
 
   constructor() { }
 
