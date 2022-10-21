15d5f914b4584a0e3393c814d40123cfbea8f1f3
TheWhiteRabbit21 <milutinovicn21@gmail.com>
2022-10-21 10:44:30 +0200

    Dodata welcome componenta

--- src/app/modules/pages/pages.module.ts
+++ src/app/modules/pages/pages.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from 'src/app/app-routing.module'; 
 import { HomeComponent } from './home/home.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
+    WelcomeComponent,
   ],
   imports: [
     CommonModule,
