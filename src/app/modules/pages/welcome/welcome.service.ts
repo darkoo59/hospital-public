2dbdcdf19c92afe267c50ffde2be0a92c2186ed3
TheWhiteRabbit21 <milutinovicn21@gmail.com>
2022-10-30 17:39:53 +0100

    Update welcome.service.ts

--- src/app/modules/pages/welcome/welcome.service.ts
+++ src/app/modules/pages/welcome/welcome.service.ts
@@ -10,32 +10,7 @@ export class WelcomePageService {
 
     constructor(private _http: HttpClient) {}
 
-    // getFeedbacks() : Observable<IFeedback[]> {
-    //     return this._http.get<IFeedback[]>(this._feedbacksPublicURL);
-
-    getFeedbacks(): IFeedback[] {
-        return [
-            {
-                "feedbackDate": Date.now(),
-                "feedbackId": 1,
-                "feedbackText": "Prvi feedback",
-                "feedbackAnonymous": false,
-                "feedbackPublic": true
-            },
-            {
-                "feedbackDate": Date.now(),
-                "feedbackId": 2,
-                "feedbackText": "Drugi feedback",
-                "feedbackAnonymous": false,
-                "feedbackPublic": true
-            },
-            {
-                "feedbackDate": Date.now(),
-                "feedbackId": 3,
-                "feedbackText": "Treci feedback",
-                "feedbackAnonymous": false,
-                "feedbackPublic": true
-            }
-        ];  //dummy podaci TODO: napraviti poziv backendu za podatke.
+    getFeedbacks() : Observable<IFeedback[]> {
+        return this._http.get<IFeedback[]>(this._feedbacksPublicURL);
     }
 }
