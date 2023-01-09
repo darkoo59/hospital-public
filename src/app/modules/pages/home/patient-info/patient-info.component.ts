9f91ed0d9587075beb7813963290a2476546ed21
MisoleM <marjanovic.ra172.2019@uns.ac.rs>
2023-01-09 13:17:01 +0100

    home component changes

--- src/app/modules/pages/home/patient-info/patient-info.component.ts
+++ src/app/modules/pages/home/patient-info/patient-info.component.ts
@@ -6,6 +6,9 @@ import { PatientInfoService } from './patient-info.service';
 export interface PatientInfo {
   name: string;
   surname: string;
+  jmbg: string;
+  email: string;
+  address: string;
 }
 
 @Component({
@@ -14,22 +17,23 @@ export interface PatientInfo {
   styleUrls: ['./patient-info.component.css']
 })
 export class PatientInfoComponent implements OnInit {
-  displayedColumns: string[] = ['name', 'surname'];
-  public patient: PatientInfo = {name:"Petar", surname:"Petrovic"};
-  public patientInfo: PatientInfo[] = [];
+  displayedColumns: string[] = ['name', 'surname', 'jmbg', 'email', 'address'];
+  public patient: PatientInfo = {name:"Petar", surname:"Petrovic", jmbg:"1205997202356", email:"markomarkovic1@gmail.com", address:"Marka Markovica 1"};
+  public patientInfo: PatientInfo[] = [{name:"Marko", surname:"Markovic", jmbg:"1205997202356", email:"markomarkovic1@gmail.com", address:"Marka Markovica 1"}];
   public dataSource = new MatTableDataSource(this.patientInfo);
 
   constructor(private _patientInfoService: PatientInfoService, private http:HttpClient) {}
 
   ngOnInit(): void {
-    this.getPatientInfo();
+    //this.getPatientInfo();
+    this.dataSource = new MatTableDataSource(this.patientInfo);
   }
 
-  public getPatientInfo(){
-    this._patientInfoService.getById().subscribe(res => {
-      this.patient = res;
-      this.patientInfo.push(this.patient);
-      this.dataSource = new MatTableDataSource(this.patientInfo);
-    })
-  }
+ // public getPatientInfo(){
+   // this._patientInfoService.getById().subscribe(res => {
+   //   this.patient = res;
+   //   this.patientInfo.push(this.patient);
+   //   this.dataSource = new MatTableDataSource(this.patientInfo);
+   // })
+ // }
 }
