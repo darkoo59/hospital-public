039822a5a9adafc813d2ccfba6c09e47402b597e
Ognjen Golubovic <golubovic.ra151.2019@uns.ac.rs>
2023-01-15 19:08:10 +0100

    Connected login to feedback component

--- src/app/modules/pages/home/model/feedback.model.ts
+++ src/app/modules/pages/home/model/feedback.model.ts
@@ -2,16 +2,17 @@ export class Feedback {
     id: number = 0;
     textt: string = '';
     anonymity: boolean = false;
-    user: string = 'Name Surname'
+    patientId: number = 0;
     privatisation: boolean = false;
     date: string = '';
+
 
     public constructor(obj?: any) {
         if (obj) {
             this.id = obj.id;
             this.textt = obj.textt;
             this.anonymity = obj.anonymity;
-            this.user = obj.user;   
+            this.patientId = obj.patientId;   
             this.privatisation = obj.privatisation;      
             this.date = obj.date;
         }
--- src/app/modules/pages/home/services/patient-schedule-appointment.service.ts
+++ src/app/modules/pages/home/services/patient-schedule-appointment.service.ts
@@ -6,7 +6,21 @@ export class EventSchedule{
   id: number = 0;
   dates: Date[] = [];
 }
-
+export class Appointment {
+  patientId: number = 0;
+  date: Date = new Date();
+  doctorId: number = 0;
+  time: string = '';
+}
+export class Specialization {
+  specializationId: number = 0;
+  name: string = '';
+}
+export class Doctor {
+  doctorId: number = 0;
+  name: string = '';
+  surname: string = '';
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -20,5 +34,16 @@ export class PatientScheduleAppointmentService {
   sendEvents(eventSchedule: EventSchedule[]): Observable<any> {
     return this.http.post<any>(this.apiHost + 'api/appointments/sendEvents', eventSchedule, {headers: this.headers});
   }
-  
+  getSpecializations(): Observable<Specialization[]> {
+    return this.http.get<Specialization[]>(this.apiHost + 'api/Specilization', {headers: this.headers});
+  }
+  getDoctors(specialization: number): Observable<Doctor[]> {
+    return this.http.post<Doctor[]>(this.apiHost + 'api/Appointments/bySpecialization?specializationId='+specialization, {headers: this.headers});
+  }
+  getAvailableAppointments(appointment : Appointment): Observable<string[]> {
+    return this.http.post<string[]>(this.apiHost + 'api/PhysicianSchedules/appointments/getAvailable', appointment, {headers: this.headers});
+  }
+  createAppointment(appointment : Appointment): Observable<any> {
+    return this.http.post<any>(this.apiHost + 'api/PhysicianSchedules/schedule', appointment, {headers: this.headers});
+  }
 }
